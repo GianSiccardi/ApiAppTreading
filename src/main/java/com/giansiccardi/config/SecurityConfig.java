2ad02ba8a81f93f749ce4af33ce7aaa8da34bd78
GianSiccardi <giansicca70@gmail.com>
2024-08-24 00:06:23 -0300

    configuracion cross

--- src/main/java/com/giansiccardi/config/SecurityConfig.java
+++ src/main/java/com/giansiccardi/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.giansiccardi.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,7 +8,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +32,24 @@ public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws
 }
 
     private CorsConfigurationSource corsConfigurationSource() {
-    return null;
+
+
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg= new CorsConfiguration();
+                cfg.setAllowedOrigins(
+                        Arrays.asList("http://localhost:5173",
+                                "http://localhost:3000")
+                );
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setExposedHeaders(Arrays.asList(("Authorization")));
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setMaxAge(3600l);
+                return cfg;
+            }
+        };
 }
 
 }
